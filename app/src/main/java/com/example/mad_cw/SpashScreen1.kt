95edd3407990fd9fbfd8dc19a9bf1b6ab66f229f
Shadhani Rushanthi <shadhanir@gmail.com>
2022-07-16 23:33:16 +0530

    MAD cw project

---
+++ app/src/main/java/com/example/mad_cw/SpashScreen1.kt
@@ -0,0 +1,11 @@
+package com.example.mad_cw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpashScreen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
