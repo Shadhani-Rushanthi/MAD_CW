bc5d2f53bad329205959f8682eaa822fa5b6d951
Shadhani Rushanthi <shadhanir@gmail.com>
2022-07-16 23:47:07 +0530

    MAD cw project

---
+++ app/src/main/java/com/example/mad_cw/LoginScreen.kt
@@ -0,0 +1,11 @@
+package com.example.mad_cw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen2)
+    }
+}
