c46a83f5559c103bae34d75ff5fafdf931525f5c
Shadhani Rushanthi <shadhanir@gmail.com>
2022-07-18 23:07:37 +0530

    MAD cw project Welcome page

--- app/src/main/java/com/example/mad_cw/SpashScreen1.kt
+++ app/src/main/java/com/example/mad_cw/SpashScreen1.kt
@@ -1,11 +1,23 @@
 package com.example.mad_cw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SpashScreen1 : AppCompatActivity() {
+
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent( this, LoginScreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 10000)
+
     }
 }
